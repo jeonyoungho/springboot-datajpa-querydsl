746285c11c2f2282f63e3690b838239322550c0d
jeon-yongho <yhjun1000@gmail.com>
2021-06-28 00:12:31 +0900

    update SpringbootDatajpaQuerydslApplication.java

--- src/main/java/com/example/springbootdatajpaquerydsl/SpringbootDatajpaQuerydslApplication.java
+++ src/main/java/com/example/springbootdatajpaquerydsl/SpringbootDatajpaQuerydslApplication.java
@@ -1,13 +1,45 @@
 package com.example.springbootdatajpaquerydsl;
 
+import com.example.springbootdatajpaquerydsl.category.Category;
+import com.example.springbootdatajpaquerydsl.product.Product;
+import com.example.springbootdatajpaquerydsl.product.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@RequiredArgsConstructor
 @SpringBootApplication
-public class SpringbootDatajpaQuerydslApplication {
+public class SpringbootDatajpaQuerydslApplication implements CommandLineRunner {
+
+    private final ProductRepository productRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDatajpaQuerydslApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Category category = Category.builder()
+                .name("notebook")
+                .build();
+
+        List<Product> products = new ArrayList<>();
+
+        for(int i=0;i<10;i++) {
+            products.add(Product.builder()
+                    .name("macbook" + i)
+                    .category(category)
+                    .price(1000)
+                    .description("it's best notebook")
+                    .manufacturer("apple")
+                    .unitInStock(10)
+                    .build());
+        }
+
+        productRepository.saveAll(products);
+    }
 }
